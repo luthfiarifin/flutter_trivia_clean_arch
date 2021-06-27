5cc3a8218741cbe9de5965ad0b89a27df495ea63
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-06-27 13:55:15 +0700

    Change failure.dart to failures.dart

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter_trivia_clean_arch/core/error/failure.dart';
+import 'package:flutter_trivia_clean_arch/core/error/failures.dart';
 import 'package:flutter_trivia_clean_arch/core/usecases/usecases.dart';
 import 'package:flutter_trivia_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:flutter_trivia_clean_arch/features/number_trivia/domain/repositories/number_trivia_repository.dart';
