a83d4aa5bdbaf8c2565e293a472c50dc1bcd77f7
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-06-27 13:55:45 +0700

    Add server, cache exception and failures

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -8,3 +8,7 @@ abstract class Failure extends Equatable {
   @override
   List<Object?> get props => [properties];
 }
+
+class ServerException extends Failure {}
+
+class CacheException extends Failure {}
