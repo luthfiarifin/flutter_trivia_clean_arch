5ccdd6835b786971545bd4cfb420813156f86319
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-07-06 09:11:11 +0700

    Change network_info.dart package name

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_trivia_clean_arch/core/error/exceptions.dart';
 import 'package:flutter_trivia_clean_arch/core/error/failures.dart';
-import 'package:flutter_trivia_clean_arch/core/platform/network_info.dart';
+import 'package:flutter_trivia_clean_arch/core/network/network_info.dart';
 import 'package:flutter_trivia_clean_arch/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:flutter_trivia_clean_arch/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:flutter_trivia_clean_arch/features/number_trivia/data/models/number_trivia_model.dart';
