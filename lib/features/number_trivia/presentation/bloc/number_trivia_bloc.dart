d079ad04fb63043108e5fd9be93c9a5bc5620c67
Luthfi Arifin <luthfiarifin0222@gmail.com>
2021-07-07 21:20:11 +0700

    Fix import

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_trivia_clean_arch/features/number_trivia/domain/entities/number_trivia.dart';
 
 part 'number_trivia_event.dart';
